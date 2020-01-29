b38f684209a83cbb25c56ffb615e1c521514184d
Pia Lyck Festersen <plf@matterpension.dk>
2020-01-29 16:11:04 +0100

    add Mongoose, bookModel and books

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
-
+const db = mongoose.connect('mongodb://localhost/bookAPI');
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
+const Book = require('./models/bookModel');
+
+bookRouter.route('/books')
+  .get((req, res) => {
+    Book.find(
+      (err, books) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(books);
+      },
+    );
+  });
+
+app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
   res.send('Welome to my Nodemon API!');
