efccedf998a0a565b97c1c9b2b0986bf9c6f0447
Pia Lyck Festersen <plf@matterpension.dk>
2020-01-29 16:53:31 +0100

    add POST + body-parser, clean code

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
 const port = process.env.PORT || 3000;
-const bookRouter = express.Router();
 const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
-bookRouter.route('/books')
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
-    Book.find(query,
-      (err, books) => {
-        if (err) {
-          return res.send(err);
-        }
-        return res.json(books);
-      });
-  });
+
 
 app.use('/api', bookRouter);
 
