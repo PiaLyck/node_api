2815b0bd83caf1f20c1a081718671e9f7c1747f7
Pia Lyck Festersen <plf@matterpension.dk>
2020-01-29 16:26:33 +0100

    add filter to get items based on query string

--- app.js
+++ app.js
@@ -9,14 +9,18 @@ const Book = require('./models/bookModel');
 
 bookRouter.route('/books')
   .get((req, res) => {
-    Book.find(
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+
+    Book.find(query,
       (err, books) => {
         if (err) {
           return res.send(err);
         }
         return res.json(books);
-      },
-    );
+      });
   });
 
 app.use('/api', bookRouter);
